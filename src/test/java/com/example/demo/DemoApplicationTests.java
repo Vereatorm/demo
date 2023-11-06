312f5f382d4dea419eb77a5a02bf170a6aba0d19
Meteor_ <15721324003@163.com>
2023-11-06 16:49:08 +0800

    111

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import java.sql.SQLException;
+
+
+@SpringBootTest
+class DemoApplicationTests {
+
+@Test
+    void t(){
+    }
+
+}
